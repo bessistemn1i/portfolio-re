8e9f9a31c57038255588396be096dd94f53216a3
bessistemn1i <bessistemn1@gmail.com>
2018-09-30 17:56:05 +0200

    initial commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+
+const dirs = {
+    pug: "./src/**/*.pug",
+    sass: "./src/**/*.scss",
+    js: "./src/js/**/*.js"
+}
+
+gulp.task("watch", function() {
+    
+    browserSync.init({
+        server:{
+            baseDir: "dist"
+        }
+    });
+
+    // pug
+    gulp.watch(dirs.pug, ["pugChanged"]);
+
+    // css
+    gulp.watch(dirs.sass, ["cssInject"]);
+
+    // js
+    gulp.watch(dirs.js, ["jsChanged"]);
+
+});
+
+
+// html
+gulp.task("pugChanged", ["pugRender"], () => {
+    browserSync.reload();
+});
+
+// styles
+gulp.task("cssInject", ["styles"], () => {
+    gulp.src("./dist/styles.css")
+        .pipe(browserSync.stream());
+});
+
+// js
+gulp.task("jsChanged", ["scripts"], () => {
+    browserSync.reload();
+});
