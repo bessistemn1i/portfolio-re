758e271bf14fc71ea316034f3111d10f7c284476
bessistemn1i <eduard.zhulega@yandex.ru>
2018-10-05 12:44:01 +0200

    add mobile menu

--- src/js/modules/MobileMenu.js
+++ src/js/modules/MobileMenu.js
@@ -5,19 +5,19 @@ class MobileMenu {
 
     events() {
         let menuIcon = document.querySelector('.mobile-icon');
-        let mobileMenu = document.querySelector('.main-nav__menu');
-        let menuLinks = document.querySelectorAll('.main-nav__menu-link');
+        let mobileMenu = document.querySelector('.menu');
+        let menuLinks = document.querySelectorAll('.menu__link');
 
         menuIcon.addEventListener('click', () => {
             menuIcon.classList.toggle('mobile-icon--active');
-            mobileMenu.classList.toggle('main-nav__menu--active');
+            mobileMenu.classList.toggle('menu--active');
         });
 
         for(let i = 0; i < menuLinks.length; i++) {
             let link = menuLinks[i];
             link.addEventListener('click', () => {
                 menuIcon.classList.remove('mobile-icon--active');
-                mobileMenu.classList.remove('main-nav__menu--active');
+                mobileMenu.classList.remove('menu--active');
             });
         }
     }
